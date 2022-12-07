35986f12478f9ad5be9042424b65952fbbb84480
Raul Abelleira <raulabelleira@gmail.com>
2022-12-07 12:19:23 -0300

    blog app

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
+    'blog',
     'rest_framework',
     'rest_framework.authtoken',
     'corsheaders',
